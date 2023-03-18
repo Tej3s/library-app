d1325a0261962e78618df3e8c9761217b7ef0bd5
Tej3s <86160715+Tej3s@users.noreply.github.com>
2023-03-18 10:45:46 -0400

    Add files via upload

---
+++ components/bottomTabNavigator.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import TransactionScreen from '../screens/transaction'
+import SearchScreen from '../screens/search'
+
+const Tab = createBottomTabNavigator()
+export default class BottomTabNavigator extends Component{
+    render(){
+        return(
+            <NavigationContainer>
+                <Tab.Navigator>
+                    <Tab.Screen name = 'transaction' component = {TransactionScreen}/>
+                    <Tab.Screen name = 'search' component = {SearchScreen}/>
+                </Tab.Navigator>
+            </NavigationContainer>
+
+        );
+    }
+}
